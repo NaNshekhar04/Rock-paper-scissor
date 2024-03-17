fb93dc358b6937f142ed24b0586049616ea62015
NaNshekhar04 <connectshekharthakur09@gmail.com>
2024-03-17 22:32:41 +0530

    minor changes : Final commit!

--- script.js
+++ script.js
@@ -1,136 +1,134 @@
-let rulesButton = document.getElementById('Rules-btn');
-let rulesBox = document.getElementById('rules-box');
-let cancelButton = document.getElementById('cancel-btn');
+let rulesButton = document.getElementById("Rules-btn");
+let rulesBox = document.getElementById("rules-box");
+let cancelButton = document.getElementById("cancel-btn");
 
-rulesButton.addEventListener('click',()=>{
-    console.log('button clicked');
-    rulesBox.style.display = 'block';
-})
+rulesButton.addEventListener("click", () => {
+    console.log("button clicked");
+    rulesBox.style.display = "block";
+});
 
-cancelButton.addEventListener('click',()=>{
-    console.log('cancel button clicked');
-    rulesBox.style.display = 'none';
-})
+cancelButton.addEventListener("click", () => {
+    console.log("cancel button clicked");
+    rulesBox.style.display = "none";
+});
 
-let userpickedRock = document.getElementById('rockbtn');
-let userpickedPaper =  document.getElementById('paperbtn');
-let userpickedScissor = document.getElementById('scissorbtn');
+let userpickedRock = document.getElementById("rockbtn");
+let userpickedPaper = document.getElementById("paperbtn");
+let userpickedScissor = document.getElementById("scissorbtn");
 
-
-userpickedRock.addEventListener('click', ()=>{
-    let displayElement = document.getElementById('you-picked');
+userpickedRock.addEventListener("click", () => {
+    let displayElement = document.getElementById("you-picked");
     displayElement.innerHTML = `<img src="Images/Rock.png" alt="Rock" srcset="">`;
 
-    let options = document.getElementById('options');
-    options.style.display = 'none';
+    let options = document.getElementById("options");
+    options.style.display = "none";
 
-    let showRound = document.getElementById('match-area');
-    showRound.style.display = 'flex';
-    showRound.style.justifyContent = 'space-around';
-    showRound.style.marginTop = '-330px';
+    let showRound = document.getElementById("match-area");
+    showRound.style.display = "flex";
+    showRound.style.justifyContent = "space-around";
+    showRound.style.marginTop = "-330px";
 
-    let showTitle = document.getElementById('match-title');
-    showTitle.style.display = 'flex';
+    let showTitle = document.getElementById("match-title");
+    showTitle.style.display = "flex";
 
-    let winbox = document.getElementById('win-box')
-    winbox.style.display = 'block'
+    let winbox = document.getElementById("win-box");
+    winbox.style.display = "block";
 
-    play()
-})
+    play();
+});
 
-
-userpickedPaper.addEventListener('click', ()=>{
-    let displayElement = document.getElementById('you-picked');
+userpickedPaper.addEventListener("click", () => {
+    let displayElement = document.getElementById("you-picked");
     displayElement.innerHTML = `<img src="Images/Paper.png" alt="Paper" srcset="">`;
 
-    let options = document.getElementById('options');
-    options.style.display = 'none';
+    let options = document.getElementById("options");
+    options.style.display = "none";
 
-    let showRound = document.getElementById('match-area');
-    showRound.style.display = 'flex';
-    showRound.style.justifyContent = 'space-around';
-    showRound.style.marginTop = '-330px';
+    let showRound = document.getElementById("match-area");
+    showRound.style.display = "flex";
+    showRound.style.justifyContent = "space-around";
+    showRound.style.marginTop = "-330px";
 
-    let showTitle = document.getElementById('match-title');
-    showTitle.style.display = 'flex';
+    let showTitle = document.getElementById("match-title");
+    showTitle.style.display = "flex";
 
-    let winbox = document.getElementById('win-box')
-    winbox.style.display = 'block'
+    let winbox = document.getElementById("win-box");
+    winbox.style.display = "block";
 
-    play()
-})
+    play();
+});
 
-
-userpickedScissor.addEventListener('click', ()=>{
-    let displayElement = document.getElementById('you-picked');
+userpickedScissor.addEventListener("click", () => {
+    let displayElement = document.getElementById("you-picked");
     displayElement.innerHTML = `<img src="Images/Scissor.png" alt="Scissor" srcset="">`;
 
-    let options = document.getElementById('options');
-    options.style.display = 'none';
+    let options = document.getElementById("options");
+    options.style.display = "none";
 
-    let showRound = document.getElementById('match-area');
-    showRound.style.display = 'flex';
-    showRound.style.justifyContent = 'space-around';
-    showRound.style.marginTop = '-330px';
+    let showRound = document.getElementById("match-area");
+    showRound.style.display = "flex";
+    showRound.style.justifyContent = "space-around";
+    showRound.style.marginTop = "-330px";
 
-    let showTitle = document.getElementById('match-title');
-    showTitle.style.display = 'flex';
+    let showTitle = document.getElementById("match-title");
+    showTitle.style.display = "flex";
 
-    let winbox = document.getElementById('win-box')
-    winbox.style.display = 'block'
+    let winbox = document.getElementById("win-box");
+    winbox.style.display = "block";
 
-    play()
-})
+    play();
+});
 
+let compicked = ["Rock", "Paper", "Scissor"];
 
-let compicked = ['Rock', 'Paper', 'Scissor'];
+compickedIndex = Math.floor(Math.random() * compicked.length);
 
-    compickedIndex = Math.floor(Math.random()* compicked.length);
+let comDisplay = document.getElementById("Pc-picked");
+let compickedRandom = compicked[compickedIndex];
 
-    let comDisplay = document.getElementById('Pc-picked');
-    let compickedRandom = compicked[compickedIndex];
-
-    if(compickedRandom == 'Rock'){
-        comDisplay.innerHTML = '<img src="Images/Rock.png" alt="Rock" srcset="">';
-    }else if(compickedRandom == 'Paper'){
-        comDisplay.innerHTML = '<img src="Images/Paper.png" alt="Paper" srcset="">';
-    }else if(compickedRandom == 'Scissor'){
-        comDisplay.innerHTML = '<img src="Images/Scissor.png" alt="Scissor" srcset="">';
-    }
-
+if (compickedRandom == "Rock") {
+    comDisplay.innerHTML = '<img src="Images/Rock.png" alt="Rock" srcset="">';
+} else if (compickedRandom == "Paper") {
+    comDisplay.innerHTML = '<img src="Images/Paper.png" alt="Paper" srcset="">';
+} else if (compickedRandom == "Scissor") {
+    comDisplay.innerHTML =
+        '<img src="Images/Scissor.png" alt="Scissor" srcset="">';
+}
 
 let userScore = 0;
 let compScore = 0;
 
-let userWinnerBackground ;
+let userWinnerBackground;
 let compWinnerBackground;
 
-let nextButton = document.getElementById('Next-btn');
+let nextButton = document.getElementById("Next-btn");
 
 function play() {
-    let playerPicked = document.getElementById("you-picked").querySelector('img').alt;
+    let playerPicked = document
+        .getElementById("you-picked")
+        .querySelector("img").alt;
 
-    let userScoreDisplay = document.getElementById('User-Score-display');
-    let compScoreDisplay = document.getElementById('Comp-Score-display');
+    let userScoreDisplay = document.getElementById("User-Score-display");
+    let compScoreDisplay = document.getElementById("Comp-Score-display");
 
-    let userWinnerBackground = document.getElementById('user-Winner-Background');
-    let compWinnerBackground = document.getElementById('comp-Winner-Background');
+    let userWinnerBackground = document.getElementById("user-Winner-Background");
+    let compWinnerBackground = document.getElementById("comp-Winner-Background");
 
-    let resultElement = document.getElementById('result');
-    let playbuttonTextChange = document.getElementById('play-again');
+    let resultElement = document.getElementById("result");
+    let playbuttonTextChange = document.getElementById("play-again");
 
     if (playerPicked === compickedRandom) {
         resultElement.innerText = "TIE";
         playbuttonTextChange.innerText = "REPLAY";
     } else if (
-        (playerPicked === 'Rock' && compickedRandom === 'Scissor') ||
-        (playerPicked === 'Paper' && compickedRandom === 'Rock') ||
-        (playerPicked === 'Scissor' && compickedRandom === 'Paper')
+        (playerPicked === "Rock" && compickedRandom === "Scissor") ||
+        (playerPicked === "Paper" && compickedRandom === "Rock") ||
+        (playerPicked === "Scissor" && compickedRandom === "Paper")
     ) {
         resultElement.innerHTML = `YOU WIN <br> <span class="txt">AGAINST PC</span>`;
         userScore++;
         userWinnerBackground.style.opacity = "100";
-        nextButton.style.display = 'inline-flex';
+        nextButton.style.display = "inline-flex";
     } else {
         resultElement.innerHTML = `YOU LOST <br> <span class="txt">AGAINST PC</span>`;
         compScore++;
@@ -143,70 +141,61 @@ function play() {
     saveScore();
 }
 
+nextButton.addEventListener("click", () => {
+    let container = document.querySelector(".container");
+    container.style.display = "none";
 
+    let showWinnerPage = document.getElementById("Winner-page");
+    showWinnerPage.style.display = "block";
 
-nextButton.addEventListener('click', ()=>{
-    let container = document.querySelector('.container')
-    container.style.display ='none';
-
-    let showWinnerPage = document.getElementById('Winner-page')
-    showWinnerPage.style.display = 'block';
-
-    let displayRulebtn = document.getElementById('Rules-btn')
-    displayRulebtn.style.display = 'block';
+    let displayRulebtn = document.getElementById("Rules-btn");
+    displayRulebtn.style.display = "block";
 });
 
+let ruleButtonWinnerPage = document.getElementById("Rules-btn-win-page");
+let cancelButtonWinnerPage = document.getElementById("cancel-btn-two");
 
+ruleButtonWinnerPage.addEventListener("click", () => {
+    let rulesBoxWinnerPage = document.getElementById("rules-box-winner-page");
+    console.log("rule button clicked");
+    rulesBoxWinnerPage.style.display = "block";
+});
+cancelButtonWinnerPage.addEventListener("click", () => {
+    let rulesBoxWinnerPage = document.getElementById("rules-box-winner-page");
+    console.log("cancel button clicked");
+    rulesBoxWinnerPage.style.display = "none";
+});
 
-let ruleButtonWinnerPage = document.getElementById('Rules-btn-win-page')
-let cancelButtonWinnerPage = document.getElementById('cancel-btn-two')
+let playagain = document.getElementById("play-again");
 
-ruleButtonWinnerPage.addEventListener('click',()=>{
-    let rulesBoxWinnerPage = document.getElementById('rules-box-winner-page');
-    console.log('rule button clicked');
-    rulesBoxWinnerPage.style.display = 'block';
-})
-cancelButtonWinnerPage.addEventListener('click',()=>{
-    let rulesBoxWinnerPage = document.getElementById('rules-box-winner-page');
-    console.log('cancel button clicked');
-    rulesBoxWinnerPage.style.display = 'none';
-})
-
-
-
-let playagain = document.getElementById('play-again');
-
-playagain.addEventListener('click',()=>{
+playagain.addEventListener("click", () => {
     location.reload();
     saveScore();
-})
+});
 
+let playagainwinnerPage = document.getElementById("play-again-btn-win-page");
 
-let playagainwinnerPage = document.getElementById('play-again-btn-win-page');
-
-playagainwinnerPage.addEventListener('click',()=>{
+playagainwinnerPage.addEventListener("click", () => {
     location.reload();
     saveScore();
-})
+});
 
-
-
-function saveScore(){
-    localStorage.setItem('userScore', userScore);
-    localStorage.setItem('compScore', compScore);
+function saveScore() {
+    localStorage.setItem("userScore", userScore);
+    localStorage.setItem("compScore", compScore);
 }
 
-function showScore(){
-    let userScoreDisplay = document.getElementById('User-Score-display');
-    let compScoreDisplay = document.getElementById('Comp-Score-display');
+function showScore() {
+    let userScoreDisplay = document.getElementById("User-Score-display");
+    let compScoreDisplay = document.getElementById("Comp-Score-display");
 
-    userScore = parseInt(localStorage.getItem('userScore')) || 0;
-    compScore = parseInt(localStorage.getItem('compScore')) || 0;
+    userScore = parseInt(localStorage.getItem("userScore")) || 0;
+    compScore = parseInt(localStorage.getItem("compScore")) || 0;
 
     userScoreDisplay.innerText = userScore;
     compScoreDisplay.innerText = compScore;
-} 
+}
 
 window.onload = function () {
     showScore();
-}
+};
